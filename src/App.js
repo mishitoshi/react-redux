b38e2383c64365eff5e687fdfd2045256b76f844
mishitokun <toshimishito000@gmail.com>
2019-08-24 18:00:06 +0900

    jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 
-function App() {
-  return <h1>Hello, world!</h1>;
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input
+          type="text"
+          onChange={() => {
+            console.log("i am clicked");
+          }}
+        />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
